fdc276c502c6c04ae43ace4b58463172c59c8362
Jefferson <jeffersonporras.jp@gmail.com>
2026-06-29 20:46:53 -0500

    feat: estructura inicial limpia de React y Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return(
+    <div style={{background: '#121212', color: '#fff', minHeight: '100vh', padding: '20px'}}>
+      <h1>CNC Assistant</h1>
+      <p>¡Solucionado! El Entorno está limppio y listo para meter los parametros.</p>
+    </div>
+  );
+}
+
+export default App;
